d28cede0c3d3671b076ddb6f32ea1ffface44879
StJohn <stjohn@me.com>
2023-11-04 17:39:37 +0000

    Styling

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,12 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection, z } from "astro:content";
 
 // 2. Define a `type` and `schema` for each collection
 const blogCollection = defineCollection({
-  type: 'content',
+  type: "content",
   schema: z.object({
     title: z.string(),
-    pubDate:z.date(),
+    pubDate: z.date(),
+    updatedDate: z.date().optional(),
     tags: z.array(z.string()),
     image: z.string().optional(),
   }),
@@ -13,5 +14,5 @@ const blogCollection = defineCollection({
 
 // 3. Export a single `collections` object to register your collection(s)
 export const collections = {
-  'blog': blogCollection,
+  blog: blogCollection,
 };
