3a881b5affda7a024befcafc62797da1acbb41d3
StJohn <stjohn@me.com>
2023-11-04 12:33:37 +0000

    Add Blog

---
+++ src/content/config.ts
@@ -0,0 +1,17 @@
+import { defineCollection, z } from 'astro:content';
+
+// 2. Define a `type` and `schema` for each collection
+const blogCollection = defineCollection({
+  type: 'content',
+  schema: z.object({
+    title: z.string(),
+    pubDate:z.date(),
+    tags: z.array(z.string()),
+    image: z.string().optional(),
+  }),
+});
+
+// 3. Export a single `collections` object to register your collection(s)
+export const collections = {
+  'blog': blogCollection,
+};
